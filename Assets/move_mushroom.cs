0af09a7ca4f4a4b15249f3933eeb954fa8a94e59
Jantsen Chalifoux <Jantsen Chalifoux@DESKTOP-V02OA89>
2020-12-12 19:45:17 -0500

    Add .gitignore and .gitattributes.

---
+++ Assets/move_mushroom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_mushroom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int speed = 1;
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 direction = input.normalized;
+        Vector2 velocity = direction * speed;
+        //print(input.x + "     " +  input.y);
+        //hghgh
+        transform.Translate(velocity*Time.deltaTime);
+    }
+}
